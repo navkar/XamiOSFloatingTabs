381f81f54d9ac1704b09e677de2650372ca20640
Naveen Karamchetti <navkar@outlook.com>
2020-09-02 17:39:35 -0500

    Initial version

---
+++ XamarinFloatingTabs/Views/DiscoverPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using XamarinFloatingTabs.Models;
+
+namespace XamarinFloatingTabs.Views
+{
+    public partial class DiscoverPage : BasePage
+    {
+        public IEnumerable<Story> Stories { get; }
+        public IEnumerable<Bird> NearbyBirds { get; }
+
+        public static ItemsLayout ListItemsLayout { get; set; }
+
+        public DiscoverPage()
+        {
+            InitializeComponent();
+
+            Stories = new Story[]
+            {
+                new Story() { Category = "The Wilds", Title = "Why we need to save the scavengers?", ImageUrl = "http://www.birdlife.org/sites/default/files/styles/1600/public/news/european_turtle_dove_streptopelia_turtur_websitec_revital_salomon.jpg" },
+                new Story() { Category = "Science", Title = "Drunk birds? What is happening...", ImageUrl = "http://www.birdlife.org/sites/default/files/styles/full_620x295/public/news/little_spiderhunter_c_noicherrybeans_shutterstock_smaller_1.jpg" },
+                new Story() { Category = "The Wilds", Title = "Why we need to save the scavengers?", ImageUrl = "http://www.birdlife.org/sites/default/files/styles/full_620x295/public/news/canada_warbler_c_jayne_gulbrand_smaller.jpg" },
+                new Story() { Category = "Science", Title = "Drunk birds? What is happening...", ImageUrl = "http://cdn.images.express.co.uk/img/dynamic/13/590x/549233_1.jpg" },
+                new Story() { Category = "The Wilds", Title = "Why we need to save the scavengers?", ImageUrl = "http://www.birdlife.org/sites/default/files/styles/full_620x295/public/news/rufous_hummingbird_selasphorus_rufus_c_ryan_bushby_1.jpg" }
+            };
+
+            NearbyBirds = new Bird[]
+            {
+                new Bird() { Name = "Eagle", ImageUrl = "https://www.birdlife.org/sites/default/files/styles/1600/public/lesser-spotted-eagle-johann-du-preez.png" },
+                new Bird() { Name = "Golden eagle", ImageUrl = "https://www.birdlife.org/sites/default/files/styles/1600/public/golden_eagle_c_marc_jones_full_size.jpg" },
+                new Bird() { Name = "Bald eagle", ImageUrl = "https://www.birdlife.org/sites/default/files/styles/1600/public/skeeze-pixabay-bald-eagle-977811_1280_2.jpg" },
+                new Bird() { Name = "Imperial eagle", ImageUrl = "https://www.birdlife.org/sites/default/files/styles/1600/public/e_imperial_eagle_flickr.jpg" }
+            };
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ XamarinFloatingTabs/Views/OverlayTabView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFloatingTabs.Views
+{
+    public partial class OverlayTabView : ContentView
+    {
+        public OverlayTabView()
+        {
+            InitializeComponent();
+            Tab1.TextColor = Color.Gray;
+            Tab2.TextColor = Tab3.TextColor = Color.DarkGray;
+        }
+
+        private void OnDiscoveryClicked(object sender, System.EventArgs e)
+        {
+            if (App.Current.MainPage is TabbedPage mainPage && sender is Button button)
+            {
+                mainPage.CurrentPage = mainPage.Children[0];
+                Tab1.TextColor = Color.Gray;
+                Tab2.TextColor = Tab3.TextColor = Color.DarkGray;
+            }
+        }
+
+        private void OnSearchClicked(object sender, System.EventArgs e)
+        {
+            if (App.Current.MainPage is TabbedPage mainPage && sender is Button button)
+            {
+                mainPage.CurrentPage = mainPage.Children[1];
+                Tab2.TextColor = Color.Gray;
+                Tab1.TextColor = Tab3.TextColor = Color.DarkGray;
+            }
+        }
+
+        private void OnBookmarksClicked(object sender, System.EventArgs e)
+        {
+            if (App.Current.MainPage is TabbedPage mainPage && sender is Button button)
+            {
+                mainPage.CurrentPage = mainPage.Children[2];
+                Tab3.TextColor = Color.Gray;
+                Tab1.TextColor = Tab2.TextColor = Color.DarkGray;
+            }
+        }
+    }
+}
